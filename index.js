97312274e6bec7108518b861b22058fbc04f5f2d
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2021-02-02 20:39:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,21 +15,6 @@ client.commands = new Collection();
 
 console.log("Ne pas oubliez ! Si une variable stocké dans la bdd est utilisé dans une commande , ne pas oubliez de mettre le Savebdd() !");
 
-bot.on("ready", async () =>{
-  console.log(`Connecté en tant que ${bot.user.tag}`);
-  bot.user.setStatus("online")//invisible dnd online idle
-  setTimeout(() => {
-    bot.user.setActivity(`${bot.guilds.cache.size} serveurs`, {type: "WATCHING"})
-    setTimeout(() => {
-      let statuts = bdd.stats
-      setInterval(function() {
-        let stats = statuts[Math.floor(Math.random()*statuts.length)];
-        bot.user.setActivity(stats, {type: "WATCHING"})
-      }, 7000)
-    }, 100)
-  }, 10000)
-});
-
 //Commandes
 
 bot.on("message", async message => {
