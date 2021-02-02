eff5a40cc3b014e614df2eb48596e2da5d38c00e
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2021-02-02 20:51:43 +0100

    Update status.js

--- status.js
+++ status.js
@@ -1,16 +1,3 @@
 module.exports = () => {
-  bot.on("ready", async () =>{
-    console.log(`Connecté en tant que ${bot.user.tag} | ${bot.guilds.cache.size} serveurs`);
-    bot.user.setStatus("online")//invisible dnd online idle
-    setTimeout(() => {
-      bot.user.setActivity(`${bot.guilds.cache.size} serveurs`, {type: "WATCHING"})
-      setTimeout(() => {
-        let statuts = bdd.stats
-       setInterval(function() {
-          let stats = statuts[Math.floor(Math.random()*statuts.length)];
-         bot.user.setActivity(stats, {type: "WATCHING"})
-        }, 7000)
-      }, 100)
-    }, 10000)
-  })
+  console.log("Bot prêt")
 }
