ca8b6aab1b8f18f5226c664cce233d2d2ed8d08e
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2021-02-03 19:26:22 +0100

    Update ready.js

--- ready.js
+++ ready.js
@@ -1,3 +1,10 @@
-module.exports = () => {
+module.exports = (bdd, bot) => {
   console.log("Bot prêt")
+    
+   let statuts = bdd.stats
+   setInterval(function() {
+       let stats = statuts[Math.floor(Math.random()*statuts.length)];
+       bot.user.setActivity(stats, {type: "STREAMING"})
+   }, 10000)
+   bot.user.setStatus("dnd");
 }
