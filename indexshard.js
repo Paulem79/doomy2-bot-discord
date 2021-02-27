3a6572de7c436cb53f17a73af090aa64c380d56b
Paulem79 <66211574+Paulem79@users.noreply.github.com>
2021-02-27 20:01:49 +0100

    Update indexshard.js

--- indexshard.js
+++ indexshard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./shard.js', {token: "NzUwMDQ2MDk5MDkxNDg4ODU5.X0003Q.shOvucW2Vz9hrRT47bNHZ6xXE30"});
+const manager = new ShardingManager('./shard.js', { token: 'NzUwMDQ2MDk5MDkxNDg4ODU5.X0003Q.shOvucW2Vz9hrRT47bNHZ6xXE30' });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
